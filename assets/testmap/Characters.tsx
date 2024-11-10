4dd3027f85b76b5f5c0765690b0601df6071499d
Zippo <moshe@zippo.io>
2024-11-10 19:22:22 +0200

    test

---
+++ assets/testmap/Characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Characters" tilewidth="32" tileheight="48" tilecount="2" columns="2">
+ <image source="Sprite-0002-Sheet.png" width="64" height="48"/>
+</tileset>
---
+++ assets/testmap/Isometric.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Isometric" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <grid orientation="isometric" width="32" height="32"/>
+ <image source="isometric tileset/spritesheet.png" width="352" height="352"/>
+</tileset>
